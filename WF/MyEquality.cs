a986a0c2fcdf3e9e5f57a73d2a12db4ee697c84e
Егор Люханов <ealuhanov@yandex.ru>
2019-03-14 16:58:35 +0600

    Stage1 - Создание структуры и представления данных
    Добавлен класс UserUr, отвечающий за Юр.лиц.
    Добавлены методы установки полей и обращения к полям SetUserUrParams и UserUrParams.
    Добавлены 2 метода сравнения экземпляров класса UserUr.
    Добавлен метод получения хеша.

--- WF/MyEquality.cs
+++ WF/MyEquality.cs
@@ -1,73 +1,149 @@
 ﻿using System.Collections.Generic;
 namespace WF
 {
-    //Проверка свойств по объекту, К КОТОРОМУ ПРИМЕНЯЕТСЯ РАСШИРЕНИЕ
-    class MyEqualityComparer1to2 : IEqualityComparer<User>
-    {
-        public bool Equals(User item1, User item2)
+    #region MyEqualityForUser
+        //Проверка свойств по объекту, К КОТОРОМУ ПРИМЕНЯЕТСЯ РАСШИРЕНИЕ
+        class MyEqualityComparer1to2 : IEqualityComparer<User>
         {
-            return item1.Equals(item2) &&
-                   item1.UserParams(15).NotInvalidText() &&
-                   item2.UserParams(15).NotInvalidText() &&
-                   item1.UserParams(22).NotInvalidText() &&
-                   item1.UserParams(23).NotInvalidText();
+            public bool Equals(User item1, User item2)
+            {
+                return item1.Equals(item2) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText() &&
+                       item1.UserParams(22).NotInvalidText() &&
+                       item1.UserParams(23).NotInvalidText();
+            }
+
+            public int GetHashCode(User obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
         }
 
-        public int GetHashCode(User obj)
+        //Проверка свойств по объекту, С КОТОРЫМ ПРОИЗВОДИТСЯ СРАВНЕНИЕ 
+        class MyEqualityComparer2to1 : IEqualityComparer<User>
         {
-            return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
-        }
-    }
+            public bool Equals(User item1, User item2)
+            {
+                return item1.Equals(item2) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(22).NotInvalidText() &&
+                       item2.UserParams(23).NotInvalidText();
+            }
 
-    //Проверка свойств по объекту, С КОТОРЫМ ПРОИЗВОДИТСЯ СРАВНЕНИЕ 
-    class MyEqualityComparer2to1 : IEqualityComparer<User>
-    {
-        public bool Equals(User item1, User item2)
-        {
-            return item1.Equals(item2) &&
-                   item1.UserParams(15).NotInvalidText() &&
-                   item2.UserParams(15).NotInvalidText() &&
-                   item2.UserParams(22).NotInvalidText() &&
-                   item2.UserParams(23).NotInvalidText();
-        }
-
-        public int GetHashCode(User obj)
-        {
-            return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
-        }
-    }
-
-    //Проверка без параметров показаний
-    class MyEqualityComparerWithoutData : IEqualityComparer<User>
-    {
-        public bool Equals(User item1, User item2)
-        {
-            return item1.Equals(item2) &&
-                   item1.UserParams(15).NotInvalidText() &&
-                   item2.UserParams(15).NotInvalidText()&&
-                   item1.UserParams(20).NotInvalidText()&&
-                   item2.UserParams(20).NotInvalidText();
+            public int GetHashCode(User obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
         }
 
-        public int GetHashCode(User obj)
+        //Проверка без параметров показаний
+        class MyEqualityComparerWithoutData : IEqualityComparer<User>
         {
-            return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
-        }
-    }
+            public bool Equals(User item1, User item2)
+            {
+                return item1.Equals(item2) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText() &&
+                       item1.UserParams(20).NotInvalidText() &&
+                       item2.UserParams(20).NotInvalidText();
+            }
 
-    //Проверка по ВСЕМ* параметрам
-    class MyEqualityComparerFull : IEqualityComparer<User>
-    {
-        public bool Equals(User item1, User item2)
-        {
-            return item1.Equals(item2, true) &&
-                   item1.UserParams(15).NotInvalidText() &&
-                   item2.UserParams(15).NotInvalidText();
+            public int GetHashCode(User obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
         }
 
-        public int GetHashCode(User obj)
+        //Проверка по ВСЕМ* параметрам
+        class MyEqualityComparerFull : IEqualityComparer<User>
         {
-            return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            public bool Equals(User item1, User item2)
+            {
+                return item1.Equals(item2, true) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText();
+            }
+
+            public int GetHashCode(User obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
         }
-    }
+        #endregion
+    //
+    //
+    #region MyEqualityForUserUr
+        //Проверка свойств по объекту, К КОТОРОМУ ПРИМЕНЯЕТСЯ РАСШИРЕНИЕ
+        class MyEqualityComparer1to2Ur : IEqualityComparer<UserUr>
+        {
+            public bool Equals(UserUr item1, UserUr item2)
+            {
+                return item1.Equals(item2) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText() &&
+                       item1.UserParams(22).NotInvalidText() &&
+                       item1.UserParams(23).NotInvalidText();
+            }
+
+            public int GetHashCode(UserUr obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
+        }
+
+        //Проверка свойств по объекту, С КОТОРЫМ ПРОИЗВОДИТСЯ СРАВНЕНИЕ 
+        class MyEqualityComparer2to1Ur : IEqualityComparer<UserUr>
+        {
+            public bool Equals(UserUr item1, UserUr item2)
+            {
+                return item1.Equals(item2) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(22).NotInvalidText() &&
+                       item2.UserParams(23).NotInvalidText();
+            }
+
+            public int GetHashCode(UserUr obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
+        }
+
+        //Проверка без параметров показаний
+        class MyEqualityComparerWithoutDataUr : IEqualityComparer<UserUr>
+        {
+            public bool Equals(UserUr item1, UserUr item2)
+            {
+                return item1.Equals(item2) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText() &&
+                       item1.UserParams(20).NotInvalidText() &&
+                       item2.UserParams(20).NotInvalidText();
+            }
+
+            public int GetHashCode(UserUr obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
+        }
+
+        //Проверка по ВСЕМ* параметрам
+        class MyEqualityComparerFullUr : IEqualityComparer<UserUr>
+        {
+            public bool Equals(UserUr item1, UserUr item2)
+            {
+                return item1.Equals(item2, true) &&
+                       item1.UserParams(15).NotInvalidText() &&
+                       item2.UserParams(15).NotInvalidText();
+            }
+
+            public int GetHashCode(UserUr obj)
+            {
+                return (obj.UserParams(1).Length + obj.UserParams(2).Length + obj.UserParams(15).Length).GetHashCode();
+            }
+        }
+    #endregion
+
 }
